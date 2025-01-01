64b731ec60bebad41cf22dad6ea7b2aae4c648ce
Nazib_Laptop <nishuptonazib@gmail.com>
2025-01-01 23:19:49 +0600

    up

--- bin/live_test_1.dart
+++ bin/live_test_1.dart
@@ -10,7 +10,7 @@ triangle(){
   print("Enter the base of triangle: ");
   double ? base = double.tryParse(stdin.readLineSync()!);
 
-  if (base == null) {
+  if (base == null || base <= 0) {
     print('Invalid input for base. Please enter a valid number.');
     return;
   }
@@ -18,7 +18,7 @@ triangle(){
   print("Enter the height of triangle: ");
   double ? height = double.tryParse(stdin.readLineSync()!);
 
-  if (height == null) {
+  if (height == null || height <= 0) {
     print('Invalid input for height. Please enter a valid number.');
     return;
   }
